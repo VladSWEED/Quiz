4632bc181f038340ff6275f904b9c29032b607b6
VladSWEED <57415244+VladSWEED@users.noreply.github.com>
2020-12-17 19:54:02 +0300

    answers

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,24 +1,19 @@
 import react from 'react'
 import s from './ActiveQuiz.module.css'
+import AnswerList from './AnswerList/AnswerList'
 
 const ActiveQuiz=(props)=>{
   return(
   <div className={s.ActiveQuiz}>
     <p className={s.Question}>
       <span>
-        <strong>
-          2
-        </strong>
-        Как дела?
+        <strong>2</strong>&nbsp;
+        {props.question}
       </span>
       <small>4 из 12</small>
     </p>
-    <ul>
-       <li>1</li>
-       <li>2</li>
-       <li>3</li>
-       <li>4</li>
-    </ul>
+    <AnswerList answers={props.answers} 
+    onAnswerClickHandler={props.onAnswerClickHandler} />
 
   </div>
   )
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -4,14 +4,32 @@ import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 
 class Quiz extends React.Component{
   state={
-    quiz:[]
+    quiz:[
+      {
+        question:'Какого цвета небо?',
+        rightAnswerId:2,
+        answers:[
+          {text:'Черный',id:1},
+          {text:'Синий',id:2},
+          {text:'Красный',id:3},
+          {text:'Зеленый',id:4}
+        ]
+      }
+    ]
+  }
+
+  onAnswerClickHandler=(answerId)=>{
+    console.log('Answer id:',answerId);
   }
   render(){
     return(
       <div className={s.Quiz}>
         <div className={s.QuizWrapper}>
-        <h1>Quiz</h1>
-          <ActiveQuiz/>
+        <h1>Ответьте на все вопросы</h1>
+          <ActiveQuiz 
+          question={this.state.quiz[0].question}
+          answers={this.state.quiz[0].answers}
+          onAnswerClickHandler={this.onAnswerClickHandler}/>
         </div>
       </div>
     )
