2fd343d4fdbc1426746fec1634d6af6d083ee29b
VladSWEED <57415244+VladSWEED@users.noreply.github.com>
2020-12-22 19:26:54 +0300

    LoaderComponent_87

---
+++ src/components/UI/Loader/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import s from './Loader.module.css'
+
+const Loader=props=>{
+  return (
+    <div className={s.center}>
+       <div className={s.Loader}></div>
+    </div>
+  )
+}
+
+export default Loader
