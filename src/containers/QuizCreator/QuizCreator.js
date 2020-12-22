0ee6a37ccc10c2f43fdad55eb6c45c8063cc3b2e
VladSWEED <57415244+VladSWEED@users.noreply.github.com>
2020-12-22 16:57:55 +0300

    CreateDataServer_85

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -5,6 +5,7 @@ import {createControl, validate,validateForm} from '../../form/formFramework'
 import Input from '../../components/UI/Input/Input'
 import Auxilliary from '../../hoc/Auxilliary/Auxilliary'
 import Select from '../../components/UI/Select/Select'
+import axios from 'axios'
 
 
 function createOptionControl(number) {
@@ -71,9 +72,19 @@ export default class QuizCreator extends React.Component{
     })
   }
 
-  createQuizHandler=event=>{
+  createQuizHandler=async event=>{
     event.preventDefault()
-    console.log(this.state.quiz);
+    try{
+      await axios.post('https://react-quiz-72779-default-rtdb.firebaseio.com/quizes.json',this.state.quiz)
+      this.setState({
+      quiz:[],
+      isFormValid:false,
+      rightAnswerId:1,
+      formControls:createFormControls()
+      })
+    } catch(e){
+      console.log(e);
+    }
   }
 
   changeHandler=(value,controlName)=>{
